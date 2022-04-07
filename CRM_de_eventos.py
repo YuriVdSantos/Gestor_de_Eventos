e9d6456ef9801c2fb34dcf2c69b492226c0b35e9
Santos <yurivdsantos@outlook.com>
2022-04-07 12:56:36 -0300

    Introdução ao CRM

---
+++ CRM_de_eventos.py
@@ -0,0 +1,18 @@
+nome = input("Nome: ")
+endereço = input("Endereço: ")
+telefone = input("Telefone: ")
+email = input("Email: ")
+empresa = input("Empresa: ")
+ramo =  input("Ramo: ")
+
+
+print("Nome: %s" % nome)
+print("Endereço: %s" % endereço)
+print("Telefone: %s" % telefone)
+print("Email: %s" % email)
+print("Empresa: %s" % empresa)
+print("Ramo: %s" % ramo)
+
+cliente_1 = {nome, endereço, telefone, email, empresa, ramo}
+
+print(cliente_1)
